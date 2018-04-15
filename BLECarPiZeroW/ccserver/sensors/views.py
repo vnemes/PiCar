4ce8507e50f8cbdbf98703db97868e193de4754e
Denis Nuțiu <denis.nutiu@gmail.com>
2018-04-15 13:42:48 +0300

    Adding new sensor placeholders

--- BLECarPiZeroW/ccserver/sensors/views.py
+++ BLECarPiZeroW/ccserver/sensors/views.py
@@ -4,6 +4,38 @@ from rest_framework import status
 from shared.permissions import PublicEndpoint
 
 
+class SpeedSensor(APIView):
+    """
+    The speed sensor controls the car's speed.
+    """
+    permission_classes = (PublicEndpoint,)
+
+    def get(self, request):
+        json = {
+            "sensor_name": "speed_sensor",
+            "x": 100,
+            "y": 100,
+            "demo": True
+        }
+        return Response(json, status=status.HTTP_200_OK)
+
+
+class UltrasonicSensor(APIView):
+    """
+    Ultrasonic sensor view gets data from the ultrasonic sensor.
+    """
+    permission_classes = (PublicEndpoint,)
+
+    def get(self, request):
+        json = {
+            "sensor_name": "ultrasonic_sensor",
+            "x": 100,
+            "y": 100,
+            "demo": True
+        }
+        return Response(json, status=status.HTTP_200_OK)
+
+
 class DemoSensor(APIView):
     """
     Just a demo sensor, demonstrating that everything is working.
