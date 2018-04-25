700fd555f6f95523bcd791f9c06808eb99e09acc
Denis Nuțiu <denis.nutiu@gmail.com>
2018-04-25 16:33:34 +0300

    Adding Ultrasonic sensor get view over DBUS

--- BLECarPiZeroW/ccserver/sensors/views.py
+++ BLECarPiZeroW/ccserver/sensors/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from shared.permissions import PublicEndpoint
 
+import dbus
+bus = dbus.SessionBus()
 
 class SpeedSensor(APIView):
     """
@@ -27,13 +29,17 @@ class UltrasonicSensor(APIView):
     permission_classes = (PublicEndpoint,)
 
     def get(self, request):
-        json = {
-            "sensor_name": "ultrasonic_sensor",
-            "x": 100,
-            "y": 100,
-            "demo": True
-        }
-        return Response(json, status=status.HTTP_200_OK)
+        try:
+            object = bus.get_object("picar.sensor.ultrasonic", "/picar/sensor/ultrasonic")
+            interface = dbus.Interface(object, "picar.sensor.ultrasonic")
+            json = {
+                "sensor_name": "ultrasonic_sensor",
+                "distance": interface.getDistance(),
+            }
+            return Response(json, status=status.HTTP_200_OK)
+        except Exception:
+            return Response({"message": "The module bus is offline!", "distance": -1},
+                            status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
 
 class DemoSensor(APIView):
