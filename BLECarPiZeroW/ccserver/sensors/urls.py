515284e50c79ae62a393427728381c12a8d2fec7
Denis Nuțiu <denis.nutiu@gmail.com>
2018-05-06 20:07:47 +0300

    Adding new route for camera

--- BLECarPiZeroW/ccserver/sensors/urls.py
+++ BLECarPiZeroW/ccserver/sensors/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from sensors.views import DemoSensor, UltrasonicSensor, SpeedSensor
+from sensors.views import DemoSensor, UltrasonicSensor, SpeedSensor, CameraSensor
 
 urlpatterns = [
     url(r'^speed/$', SpeedSensor.as_view(), name="speed-sensor"),
+    url(r'^camera/$', CameraSensor.as_view(), name="camera-sensor"),
     url(r'^ultrasonic/$', UltrasonicSensor.as_view(), name="ultrasonic-sensor"),
     url(r'^demo/$', DemoSensor.as_view(), name="demo-sensor"),
 ]
