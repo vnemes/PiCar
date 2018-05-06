490deef6c33dedc78af2f08505f4c09adfa895e6
vendettavn <vladnemes5@gmail.com>
2018-05-06 21:06:21 +0300

    Implemented speed & steering http wrappers

---
+++ BLECar/app/src/main/java/vendetta/blecar/controllers/SteeringController.java
@@ -0,0 +1,59 @@
+package vendetta.blecar.controllers;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import vendetta.blecar.ControllerActivity;
+import vendetta.blecar.http.HTTPHandlerSingleton;
+import vendetta.blecar.R;
+
+/**
+ * Created by Vendetta on 06-May-18.
+ */
+
+public class SteeringController {
+
+    private Context context;
+    private static JSONObject jsonObject = new JSONObject();
+
+    private int lastSteerStr;
+    private int lastSteerAngl;
+
+    public SteeringController(Context context) {
+        this.context = context;
+        lastSteerStr = 0;
+        lastSteerAngl = 0;
+        setSteering(0, 0);
+    }
+
+    public void setSteeringAnglStr(int angle, int strength) {
+
+        if (strength == lastSteerStr && angle == lastSteerAngl)
+            return;
+
+        int direction = Math.cos(Math.toRadians(angle)) >= 0 ? 0 : 1;
+        lastSteerStr = strength;
+        lastSteerAngl = angle;
+
+        setSteering(strength, direction);
+
+        ((ControllerActivity) context).updateCrtSteeringTV(direction == 0 ? strength : -strength);
+    }
+
+    private void setSteering(int steering, int direction) {
+        try {
+            jsonObject.put("steering", steering);
+            jsonObject.put("direction", direction);
+            Log.d(getClass().getSimpleName(), jsonObject.toString());
+            HTTPHandlerSingleton.getInstance(context).addToRequestQueue(new JsonObjectRequest(Request.Method.POST, context.getString(R.string.pi_url) + "/control/steering/", jsonObject, null, null));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
