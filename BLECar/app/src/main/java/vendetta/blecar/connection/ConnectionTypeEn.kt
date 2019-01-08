b82956db43807938adbbb2cc218a2ca20d69278a
vnemes <vladnemes5@gmail.com>
2019-01-08 21:55:22 +0200

    Added image logos for ble and mqtt connection types

--- BLECar/app/src/main/java/vendetta/blecar/connection/ConnectionTypeEn.kt
+++ BLECar/app/src/main/java/vendetta/blecar/connection/ConnectionTypeEn.kt
@@ -6,6 +6,6 @@ enum class ConnectionTypeEn(val value:String, val specific: String, val img: Int
     WIFI_LOCAL("LAN Wi-Fi Connection", "Local IP Address:", R.drawable.wifi_local_logo),
     WIFI_INET("Global Wi-Fi Connection", "Internet IP Address:", R.drawable.wifi_global_logo),
     WIFI_AP("Direct Wi-Fi Access Point", "SSID:", R.drawable.wifi_ap_logo),
-    MQTT("Message Queuing Telemetry Transport", "Shared Key:", R.drawable.wifi_local_splashscreen),
-    BLE("Bluetooth Low Energy", "Beacon Name:", R.drawable.wifi_local_splashscreen)
+    MQTT("Message Queuing Telemetry Transport", "Shared Key:", R.drawable.wifi_local_mqtt),
+    BLE("Bluetooth Low Energy", "Beacon Name:", R.drawable.wifi_local_ble)
 }
