a3bfc9dc2da5d98af1e0795128bed501b9770364
vnemes <vladnemes5@gmail.com>
2019-03-16 19:54:57 +0200

    Refactored Shelby platform speed controller to use pigpio

--- BLECarPiZeroW/components/drivers/ShelbyGT500/HBridgeSpeedDriver.py
+++ BLECarPiZeroW/components/drivers/ShelbyGT500/HBridgeSpeedDriver.py
@@ -1,9 +1,8 @@
-import RPi.GPIO as gpio
+import pigpio
 from components.AbstractComponent import AbstractComponent
 
 
 class HBridgeSpeedDriver(AbstractComponent):
-
     __instance = None
     BCM_PIN_SPEED_HIGH = 19
     BCM_PIN_SPEED_LOW = 16
@@ -22,16 +21,17 @@ class HBridgeSpeedDriver(AbstractComponent):
             raise Exception("This class is a singleton!")
         else:
             HBridgeSpeedDriver.__instance = self
+            self.pi = None
         return
 
     def set_speed(self, direction, speed):
-        # speed = self.__normalize(speed)
+        speed = self.__normalize(speed)
         self.set_speed_drv(direction, speed)
         return
 
     def set_speed_drv(self, direction, speed):
-        self.speed_forward_pwm.ChangeDutyCycle(speed if direction else 0)
-        self.speed_backward_pwm.ChangeDutyCycle(0 if direction else speed)
+        self.pi.set_PWM_dutycycle(self.BCM_PIN_SPEED_HIGH, speed if direction else 0)
+        self.pi.set_PWM_dutycycle(self.BCM_PIN_SPEED_LOW, 0 if direction else speed)
         print('speed:\t' + str(speed) + (' forward' if direction == 1 else ' backward'))
         return
 
@@ -41,18 +41,18 @@ class HBridgeSpeedDriver(AbstractComponent):
         return value
 
     def start(self):
-        gpio.setmode(gpio.BCM)
-        output_channels = [self.BCM_PIN_SPEED_HIGH, self.BCM_PIN_SPEED_LOW]
-        gpio.setup(output_channels, gpio.OUT)
-        self.speed_forward_pwm = gpio.PWM(self.BCM_PIN_SPEED_HIGH, self.GPIO_PWM_FREQUENCY)
-        self.speed_backward_pwm = gpio.PWM(self.BCM_PIN_SPEED_LOW, self.GPIO_PWM_FREQUENCY)
-        self.speed_forward_pwm.start(0)
-        self.speed_backward_pwm.start(0)
+        self.pi = pigpio.pi()
+        self.pi.set_PWM_range(self.BCM_PIN_SPEED_HIGH, 100)
+        self.pi.set_PWM_range(self.BCM_PIN_SPEED_LOW, 100)
+        self.pi.set_PWM_frequency(self.BCM_PIN_SPEED_HIGH, self.GPIO_PWM_FREQUENCY)
+        self.pi.set_PWM_frequency(self.BCM_PIN_SPEED_LOW, self.GPIO_PWM_FREQUENCY)
         print('Initialized HBridgeSpeedDriver with PWM frequency of ' + str(self.GPIO_PWM_FREQUENCY) + ' Hz')
         return
 
     def stop(self):
-        self.speed_forward_pwm.stop()
-        self.speed_backward_pwm.stop()
+        self.pi.set_PWM_dutycycle(self.BCM_PIN_SPEED_HIGH, 0)
+        self.pi.set_PWM_dutycycle(self.BCM_PIN_SPEED_LOW, 0)
+        self.pi.stop()
+        self.pi = None
         print('Closed HBridgeSpeedDriver handler')
         return
